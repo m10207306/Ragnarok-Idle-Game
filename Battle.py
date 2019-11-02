b5e532d7385a2e8bdedc2ef72844cb484d529794
garyliang <garyliang@ixensor.com>
2019-11-02 16:32:21 +0800

    Refactor
    盡量使用Rect.center來指定位置，就不用加加減減

--- Battle.py
+++ Battle.py
@@ -28,9 +28,7 @@ class BattleControl:
             idx = self.standby()
 
     def standby(self):
-        default_width = 200
-        char_pos = (self.window.width * 0.4 - default_width / 2,
-                    self.window.height * 0.55 - default_width / 2)
+        char_pos = (self.window.width * 0.4, self.window.height * 0.55)
         char_animate = Animate(self.window, self.char.standby_img_path, char_pos, (0, 0))
         while True:
             self.window.tick(self.standby_fps)
@@ -45,13 +43,10 @@ class BattleControl:
     def attack(self):
         self.window.reset_chat_message()
         count = 1
-        default_width = 200
-        char_pos = (self.window.width * 0.4 - default_width / 2,
-                    self.window.height * 0.55 - default_width / 2)
-        enemy_pos = (self.window.width * 0.6 - default_width / 2,
-                     self.window.height * 0.55 - default_width / 2)
-        char_animate = Animate(self.window, self.char.attack_img_path, char_pos, (enemy_pos[0] + default_width / 2, enemy_pos[1] - 5))
-        enemy_animate = Animate(self.window, self.enemy.attack_img_path, enemy_pos, (char_pos[0] + default_width / 2, char_pos[1] - 5))
+        char_center_pos = (self.window.width * 0.4, self.window.height * 0.55)
+        enemy_center_pos = (self.window.width * 0.6, self.window.height * 0.55)
+        char_animate = Animate(self.window, self.char.attack_img_path, char_center_pos, (enemy_center_pos[0], enemy_center_pos[1] - 110))
+        enemy_animate = Animate(self.window, self.enemy.attack_img_path, enemy_center_pos, (char_center_pos[0], char_center_pos[1] - 110))
         char_move_frq = math.floor(self.attack_fps / self.char.attribute.att_frq / char_animate.image_count)
         enemy_move_frq = math.floor(self.attack_fps / self.enemy.attribute.att_frq / enemy_animate.image_count)
 
@@ -65,15 +60,15 @@ class BattleControl:
 
             if count % char_move_frq == 0:
                 damage = random.randint(1000, 1000000)
-                char_animate.update_with_sound(['角色攻擊傷害：' + str(damage)], [Blue], damage)
+                char_animate.update_with_sound(damage)
             if count % enemy_move_frq == 0:
                 damage = random.randint(5, 15)
-                enemy_animate.update_with_sound(['怪物攻擊傷害：' + str(damage)], [Red], damage)
+                enemy_animate.update_with_sound(damage)
             count += 1
 
 
 class Animate:
-    def __init__(self, graphic_obj, standby_image_path, pos, damage_pos):
+    def __init__(self, graphic_obj, standby_image_path, center_pos, damage_center_pos):
         self.window = graphic_obj
         self.default_width = 200
         self.image = self.window.load_image(standby_image_path)
@@ -81,17 +76,17 @@ class Animate:
         self.image_count = self.image.get_size()[0] / self.default_width
         self.current = 0
         self.image_array = []
-        self.pos = pos
-        self.damage_pos = damage_pos
+        self.center_pos = center_pos
+        self.damage_center_pos = damage_center_pos
         for i in range(1, int(self.image_count)+1):
             self.image_array.append(self.image.subsurface(self.window.create_rect((i-1) * self.default_width,
                                                           0,
                                                           self.default_width,
                                                           self.default_width)))
-        self.sub_bg = self.window.background.subsurface(self.window.create_rect(pos[0], pos[1],
-                                                                                self.default_width,
-                                                                                self.default_width))
-        self.damage_control = Damage(graphic_obj, self.damage_pos)
+        rect = self.window.create_rect(0, 0, self.default_width, self.default_width)
+        rect.center = self.center_pos
+        self.sub_bg = self.window.background.subsurface(rect)
+        self.damage_control = Damage(graphic_obj, self.damage_center_pos)
 
     def update(self):
         # for standby
@@ -100,33 +95,33 @@ class Animate:
         self.current += 1
         self.blit_screen()
 
-    def update_with_sound(self, text, color, damage):
+    def update_with_sound(self, damage):
         # for battle
         if self.current == self.image_count:
             self.current = 0
             self.window.effect_sound(os.path.join("Effect_Sound", "_hit_dagger.wav"))
             surface = self.damage_control.generate_damage(damage)
-            width, height = surface.get_size()
-            pos = (self.damage_pos[0] - width/2, self.damage_pos[1] - height/2)
-            self.window.screen.blit(self.window.background.subsurface(self.window.create_rect(pos[0], pos[1], width, height)),
-                                    pos)
-            self.window.screen.blit(surface, pos)
+            rect = surface.get_rect()
+            rect.center = self.damage_center_pos
+            self.window.screen.blit(surface, rect)
         self.current += 1
         self.blit_screen()
 
     def blit_screen(self):
         ani_img = self.image_array[self.current-1]
-        self.window.screen.blit(self.sub_bg, self.pos)
-        self.window.screen.blit(ani_img, self.pos)
-        self.window.update_screen((self.pos[0], self.pos[1], self.default_width, self.default_width))
+        rect = self.window.create_rect(0, 0, self.default_width, self.default_width)
+        rect.center = self.center_pos
+        self.window.screen.blit(self.sub_bg, rect)
+        self.window.screen.blit(ani_img, rect)
+        self.window.update_screen(rect)
 
 
 class Damage:
-    def __init__(self, graphic_obj, blit_pos):
+    def __init__(self, graphic_obj, blit_center_pos):
         self.window = graphic_obj
         self.width = 10
         self.height = 13
-        self.pos = blit_pos
+        self.center_pos = blit_center_pos
         self.damage_list = []
         damage_img = self.window.load_image(os.path.join("Info_Image", "Damage.png"))
         damage_img.set_colorkey(damage_img.get_at((0, 0)))
@@ -135,21 +130,21 @@ class Damage:
 
     def generate_damage(self, damage):
         damage = 9999999 if damage > 9999999 else damage
-        damage_value = [int(d) for d in str(damage)]     # 擷取傷害的每個數字
+        damage_value = [int(d) for d in str(damage)]                        # 擷取傷害的每個數字
         bg = self.window.background.copy()
-        rect = self.window.create_rect(0, 0, 7 * self.width, self.height)   # 預先撈出傷害所需要的最大畫面空間 (9999999)
-        rect.center = self.pos                           # pos是指這個畫面的中心位置
-        surface = bg.subsurface(rect)                    # surface 是最大傷害畫面區域的背景
+        rect = self.window.create_rect(0, 0, 7 * self.width, self.height)   # 預先撈出傷害所需要的最大畫面空間 (9999999) 為了避免顯示大的數字後顯示小數字會沒有把前一個數字蓋乾淨
+        rect.center = self.center_pos                                       # pos是指這個畫面的中心位置
+        surface = bg.subsurface(rect)                                       # surface 是最大傷害畫面區域的背景
 
         bg2 = self.window.background.copy()
         rect2 = self.window.create_rect(0, 0, len(damage_value) * self.width, self.height)
-        rect2.center = self.pos
-        dmg_surface = bg2.subsurface(rect2)             # dmg_surface 是實際傷害的圖像，接著要把這兩個併起來
+        rect2.center = self.center_pos
+        dmg_surface = bg2.subsurface(rect2)                                 # dmg_surface 是實際傷害的圖像，接著要把這兩個併起來
         for i in range(len(damage_value)):
             dmg_surface.blit(self.damage_list[damage_value[i]], (i * self.width, 0))
 
-        rect.topleft = (0, 0)                           # 在blit時，總是以該圖自己內部的座標來指定，左上角總是0, 0
-        rect2.center = rect.center                      # 把surface與dmg_surface的中心點對齊後畫上去
+        rect.topleft = (0, 0)                                               # 在blit時，總是以該圖自己內部的座標來指定，左上角總是0, 0
+        rect2.center = rect.center                                          # 把surface與dmg_surface的中心點對齊後畫上去
         surface.blit(dmg_surface, rect2)
         return surface
 
